754e89dede59e3e0f12134c9ea449e24d63b50d6
ThomasVisvader <thomasvisvader@gmail.com>
2023-08-09 18:46:58 +0200

    simple patrol

--- Assets/Scripts/script.cs
+++ Assets/Scripts/script.cs
@@ -1,18 +1,49 @@
 using System.Collections;
-using System.Collections.Generic;
+using Pathfinding;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
-public class script : MonoBehaviour
+public class Script : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private IAstarAI _ai;
+    // private GridGraph _grid;
+    public Tilemap tilemap;
+    private float _delta;
+    private const float DelayInSeconds = 2.0f;
+    
     void Start()
     {
-        
+        _ai = GetComponent<IAstarAI>();
+        transform.position = PickRandomTile();
+        _delta = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Update the destination of the AI if
+        // the AI is not already calculating a path and
+        // the ai has reached the end of the path or it has no path at all
+        if (!_ai.pathPending && (_ai.reachedEndOfPath || !_ai.hasPath))
+        {
+            if (_delta > DelayInSeconds)
+            {
+                _delta = 0.0f;
+                _ai.destination = PickRandomTile();
+                _ai.SearchPath();
+            }
+            else
+            {
+                _delta += Time.deltaTime;
+            }
+        }
+    }
+
+    Vector3 PickRandomTile()
+    {
+        var index = Random.Range(0, tilemap.transform.childCount);
+        var randomTile = tilemap.transform.GetChild(index).gameObject;
+        return randomTile.transform.position;
     }
 }
